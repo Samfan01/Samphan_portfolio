c7c7101da59d790fc341f53d5d539fbb07db4bca
samfan01 <dsamfan@gmail.com>
2020-12-04 17:40:11 +0300

    portfolio initial commit

---
+++ myScript.js
@@ -0,0 +1,19 @@
+var tabButtons=document.querySelectorAll(".menu .menuitem button");
+var itempanel=document.querySelectorAll(".menu .itempanel");
+
+function showpanel(panelIndex,colorCode) {
+    tabButtons.forEach(function(node){
+    node.style.backgroundColor="";
+    node.style.color="";
+    });
+    tabButtons[panelIndex].style.backgroundcolor=colorCode;
+    tabButtons[panelIndex].style.color="white";
+
+    itempanel.forEach(function(node){
+        node.style.display="none";
+});
+
+    itempanel[panelIndex].style.display="block";
+    itempanel[panelIndex].style.backgroundcolor=colorCode;
+
+} 
